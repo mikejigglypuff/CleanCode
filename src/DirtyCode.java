fc4db62e77e646be758194f2ebeed36b2aaf72ff
mikejigglypuff <k24555569@gmail.com>
2024-12-10 17:00:10 +0900

    🏭: 함수 분리 및 네이밍 변경

--- src/DirtyCode.java
+++ src/DirtyCode.java
@@ -3,20 +3,23 @@ import java.util.List;
 
 public class DirtyCode {
     public static void main(String[] args) {
+        // 현재 예약 가능한 좌석
         List<String> seats = new ArrayList<>();
         seats.add("A1:AVAILABLE");
         seats.add("A2:AVAILABLE");
         seats.add("A3:AVAILABLE");
 
+        // 좌석 예약 신청 목록
         List<String> seats2 = List.of("A2", "A3", "A1", "A4");
 
         for (String seat2 : seats2) {
+            // 예약 가능 여부
             boolean book = false;
 
             for (int i = 0; i < seats.size(); i++) {
                 String seat = seats.get(i);
-                if (seat.startsWith(seat2)) {
-                    if (seat.endsWith("BOOKED")) {
+                if (seat.startsWith(seat2)) { // 일치하는 좌석 확인
+                    if (seat.endsWith("BOOKED")) { // 이미 예약되어 있는 경우
                         System.out.println("Seat " + seat2 + " is already booked.");
                         book = true;
                         break;
@@ -34,6 +37,7 @@ public class DirtyCode {
             }
         }
 
+        // 총 예약된 좌석 수 카운트
         int count = 0;
         for (String seat : seats) {
             if (seat.endsWith("BOOKED")) {
---
+++ src/cleanupV1/DirtyCode.java
@@ -0,0 +1,48 @@
+package cleanupV1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirtyCode {
+    public static void main(String[] args) {
+        List<String> seats = new ArrayList<>();
+        seats.add("A1:AVAILABLE");
+        seats.add("A2:AVAILABLE");
+        seats.add("A3:AVAILABLE");
+
+        List<String> bookSeats = List.of("A2", "A3", "A1", "A4");
+
+        for (String bookSeat : bookSeats) {
+            System.out.println(bookSeat(seats, bookSeat));
+        }
+
+        System.out.println("Total booked seats: " + countBookedSeats(seats));
+    }
+
+    private static String bookSeat(List<String> seats, String bookSeatNum) {
+        for (int i = 0; i < seats.size(); i++) {
+            String seat = seats.get(i);
+            if (seat.startsWith(bookSeatNum)) {
+                if (seat.endsWith("BOOKED")) {
+                    return "Seat " + bookSeatNum + " is already booked.";
+                } else {
+                    seats.set(i, bookSeatNum + ":BOOKED");
+                    return "Seat " + bookSeatNum + " has been successfully booked.";
+                }
+            }
+        }
+
+        return "Seat " + bookSeatNum + " does not exist.";
+    }
+
+    private static int countBookedSeats(List<String> seats) {
+        int count = 0;
+        for (String seat : seats) {
+            if (seat.endsWith("BOOKED")) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+}
